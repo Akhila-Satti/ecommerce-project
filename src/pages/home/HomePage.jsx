ea4ae911dd64b7413af6138c110d9e0f6277d129
Akhila0929 <akhilasatti09@gmail.com>
2026-03-10 21:02:36 +0530

    updating quantity

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 import { Header } from "../../components/header";
 import "./HomePage.css";
 import { ProductsGrid } from "./ProductsGrid";
-export function HomePage({ cart }) {
+export function HomePage({ cart ,loadCart}) {
   const [products, setProducts] = useState([]);
   useEffect(() => {
     const getHomeData = async () => {
@@ -18,7 +18,7 @@ export function HomePage({ cart }) {
       <title>Ecommerce Project</title>
       <Header cart={cart} />
       <div className="home-page">
-        <ProductsGrid products={products} />
+        <ProductsGrid products={products} loadCart={loadCart}/>
       </div>
     </>
   );
