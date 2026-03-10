98478c1b56cf2bcaee0000b51d2b2bfa01a2701d
Akhila0929 <akhilasatti09@gmail.com>
2026-03-10 21:24:55 +0530

    updating delivery options

--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -4,7 +4,7 @@ import "./checkout-header.css";
 import "./CheckoutPage.css";
 import { PaymentSummary } from "./PaymentSummary";
 import { OrderSummary } from "./OrderSummary";
-export function CheckoutPage({ cart }) {
+export function CheckoutPage({ cart ,loadCart}) {
   const [deliveryOptions, setDeliveryOptions] = useState([]);
   const [paymentSummary, setPaymentSummary] = useState(null);
   useEffect(() => {
@@ -13,12 +13,13 @@ export function CheckoutPage({ cart }) {
         "/api/delivery-options?expand=estimatedDeliveryTime",
       );
       setDeliveryOptions(response.data);
+
       response = await axios.get("/api/payment-summary");
       setPaymentSummary(response.data);
     };
 
     fetchCheckoutData();
-  }, []);
+  }, [cart]);
   return (
     <>
       <title>Checkout</title>
@@ -49,8 +50,8 @@ export function CheckoutPage({ cart }) {
         <div className="page-title">Review your order</div>
 
         <div className="checkout-grid">
-          <OrderSummary cart={cart} deliveryOptions={deliveryOptions} />
-          <PaymentSummary paymenySummary={paymentSummary} />
+          <OrderSummary cart={cart} deliveryOptions={deliveryOptions} loadCart={loadCart}/>
+          <PaymentSummary paymentSummary={paymentSummary} />
         </div>
       </div>
     </>
