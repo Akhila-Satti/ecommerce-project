9493cceffa8c55f318113085c46bbd81ac577234
Akhila0929 <akhilasatti09@gmail.com>
2026-03-10 19:32:14 +0530

    async await

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,23 +1,24 @@
-import axios from 'axios';
-import { useEffect,useState } from 'react';
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Header } from "../../components/header";
 import "./HomePage.css";
-import { ProductsGrid } from './ProductsGrid';
-export function HomePage({cart}) {
-    const [products,setProducts]=useState([]);
-    useEffect(()=>{
-        axios.get('/api/products')
-            .then((response)=>{
-                setProducts(response.data);
-        });
-        
-    },[]);
+import { ProductsGrid } from "./ProductsGrid";
+export function HomePage({ cart }) {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const getHomeData = async () => {
+      const response = await axios.get("/api/products");
+      setProducts(response.data);
+    };
+    getHomeData();
+  }, []);
+
   return (
     <>
       <title>Ecommerce Project</title>
-      <Header cart={cart}/>
+      <Header cart={cart} />
       <div className="home-page">
-        <ProductsGrid products={products}/>
+        <ProductsGrid products={products} />
       </div>
     </>
   );
